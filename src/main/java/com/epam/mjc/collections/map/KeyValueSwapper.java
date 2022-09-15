c4cc0603188e50f1aa5f3b0708904c87a3ed54ee
Sasha-PC\Sasha <aleksandr.v.g.m@gmail.com>
2022-09-15 13:31:42 +0300

    Stage#1. Module #4. Map Interface Tasks.

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,18 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> swapping = new HashMap<>();
+
+        for (Map.Entry<Integer, String> entry: sourceMap.entrySet()){
+            if (swapping.containsKey(entry.getValue())){
+                swapping.put(entry.getValue(), Math.min(swapping.get(entry.getValue()), entry.getKey()));
+            }
+            else swapping.put(entry.getValue(), entry.getKey());
+        }
+        return swapping;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,29 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
+    private static final String regex = "\\s*[^(a-zA-Z_0-9)]+\\s*";
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        if (!validate(sentence))
+            return new HashMap<>();
+
+        String[] words = sentence.toLowerCase().split(regex);
+
+        Map<String, Integer> duplicateWordCounter = new HashMap<>();
+        for (String word : words) {
+            if (word.isEmpty())
+                continue;
+            if (duplicateWordCounter.containsKey(word)) {
+                int count = duplicateWordCounter.get(word);
+                duplicateWordCounter.put(word, ++count);
+            } else duplicateWordCounter.put(word, 1);
+        }
+        return duplicateWordCounter;
+    }
+
+    private boolean validate(String sentence) {
+        return !(sentence == null || sentence.isEmpty());
     }
 }
